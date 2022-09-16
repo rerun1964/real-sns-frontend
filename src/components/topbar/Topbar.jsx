be53ae7a4168687533dfc3ec908d14b05c1820d0
rerun1964 <111833758+rerun1964@users.noreply.github.com>
2022-09-16 23:36:02 +0900

    Topbar　調整　アイコン並列化

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import { Chat, Notifications, Search } from '@mui/icons-material';
+import React from 'react';
+import "./Topbar.css";
 
 export default function Topbar() {
     return (
-        <div className="topbaContainer">
+        <div className="topbarContainer">
             <div className="topbarLeft">
                 <span className="logo">Real SNS</span>
             </div>
             <div className="topbarCenter">
                 <div className="searchbar">
+                    <Search className="searchIcon" />
                     <input 
                         type="text" 
                         className="searchInput" 
@@ -15,9 +18,17 @@ export default function Topbar() {
                     />
                 </div>
             </div>
-            <div className="topbaRight">
-                <div className="topbarIconItem">1</div>
-                <div className="topbarIconItem">2</div>
+            <div className="topbarRight">
+                <div className="topbarItemIcons">
+                    <div className="topbarIconItem">
+                        <Chat />
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <Notifications />
+                        <span className="topbarIconBadge">2</span>
+                    </div>
+                </div>
             </div>
             <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
         </div>
