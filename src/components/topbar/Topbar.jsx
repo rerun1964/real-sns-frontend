6e5e6cb8aed7a9242400c9e31857d1fa3e8f3d3f
rerun1964 <111833758+rerun1964@users.noreply.github.com>
2022-09-17 00:01:06 +0900

    Topbar　スタイル完成

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -28,9 +28,10 @@ export default function Topbar() {
                         <Notifications />
                         <span className="topbarIconBadge">2</span>
                     </div>
+                    <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
                 </div>
             </div>
-            <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+            
         </div>
     );
 }
