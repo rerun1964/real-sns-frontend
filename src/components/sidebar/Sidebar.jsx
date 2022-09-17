70175db14b00ddb1a54f47bc9130a81000461183
rerun1964 <111833758+rerun1964@users.noreply.github.com>
2022-09-17 14:58:00 +0900

    タイムライン＋ライトバー　配置

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -59,6 +59,6 @@ export default function Sidebar() {
           </li>
         </ul>
       </div>
-    </div>  
-  )
+    </div>
+  );
 }
--- src/components/timeline/TimeLine.jsx
+++ src/components/timeline/TimeLine.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from 'react';
+import "./TimeLine.css";
 
 export default function TimeLine() {
   return (
-    <div>TimeLine</div>
-  )
+    <div className="timeline">
+      <div className="timelineWrapper">TimeLine
+        {/* <Share /> */}
+        {/* <Post /> */}
+      </div>
+    </div>
+  );
 }
