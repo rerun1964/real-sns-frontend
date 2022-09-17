4ddf74c29c4a7731ac3df648217782ab5a018972
rerun1964 <111833758+rerun1964@users.noreply.github.com>
2022-09-18 00:38:29 +0900

    L60. ダミーデータ(2) ユーザ名＋写真

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,15 +1,24 @@
 import { MoreVert } from '@mui/icons-material';
 import React from 'react'
 import "./Post.css";
+import { Users } from "../../dummyData";
 
 export default function Post( { post } ) {
+    // const user = Users.filter( (user) => user.id ===1 );
+    // console.log( user[0].username );
   return (
     <div className="post">
         <div className="postWrapper">
             <div className="postTop">
                 <div className="postTopLeft">
-                    <img src="./assets/person/1.jpeg" alt="" className="postProfileImg" />
-                    <span className="postUsername">Shin Code</span>
+                    <img 
+                        src={Users.filter( (user) => user.id === post.id )[0].profilePicture}
+                        alt="" 
+                        className="postProfileImg" 
+                    />
+                    <span className="postUsername">
+                        {Users.filter( (user) => user.id === post.id )[0].username}
+                    </span>
                     <span className="postDate">{post.date}</span>
                 </div>
                 <div className="postTopRight">
