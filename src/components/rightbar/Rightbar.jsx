ef10c058e60bd52f74e748366797a42cf9d17b1c
rerun1964 <111833758+rerun1964@users.noreply.github.com>
2022-09-17 22:56:40 +0900

    L55. RightBar CSS

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -4,22 +4,29 @@ import "./Rightbar.css"
 export default function Rightbar() {
   return (
     <div className="rightbar">
-      <div className="rightbarWrppaer">
+      <div className="rightbarWrapper">
         <div className="eventCountainer">
           <img src="assets/star.png" alt="" className="starImg"/>
           <span className="evenText">
             <b>フォロワー限定</b>イベント開催中！
           </span>
           </div> 
-          <img src="assets/event.jpeg" alt="" />
+          <img className="eventImg" src="assets/event.jpeg" alt="" />
           <h4 className="rightbarTitle">オンラインの友達</h4>
-          <ul className="rightbarFrinedList">
+          <ul className="rightbarFriendList">
             <li className="rightbarFriend">
               <div className="rightbarProfileImgContainer">
-                <img src="assets/person/1.jpeg" alt="" />
+                <img className="rightbarProfileImg" src="assets/person/1.jpeg" alt="" />
                 <span className="rightbarOnline"></span>
               </div>
-              <span className="rightbarUserName">Shin Code</span>
+              <span className="rightbarUsername">Shin Code</span>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img  className="rightbarProfileImg" src="assets/person/2.jpeg" alt="" />
+                <span className="rightbarOnline"></span>
+              </div>
+              <span className="rightbarUsername">Tanaka</span>
             </li>
           </ul>
       </div>
