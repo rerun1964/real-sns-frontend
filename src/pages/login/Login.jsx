d95c348c5260b41a1690e2784901bc106e2809ae
rerun1964 <111833758+rerun1964@users.noreply.github.com>
2022-09-18 14:31:13 +0900

    L73. ログインコンポーネント HTML

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -10,7 +10,17 @@ export default function Login() {
                     <h3 className="loginLogo">Real SNS</h3>
                     <span className="loginDesc">本格的なSNSを、自分の手で。</span>
                 </div>
-                <div className="loginRight">くわせふじこ</div>
+                <div className="loginRight">
+                    <div className="loginBox">
+                        <p className="loginMsg">ログインはこちら</p>
+                        <input type="text" className="loginInput" placeholder="Eメール" />
+                        <input type="text" className="loginInput" placeholder="パスワード" />
+                        <button className="loginButton" >ログイン</button>
+                        <span className="loginForgot">パスワードを忘れた方へ</span>
+                        <button className="loginREgisterButton">アカウント作成</button>
+
+                    </div>
+                </div>                
             </div>
         </div>
     </div>
