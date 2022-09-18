ecc53c9233101a0a6696fbdd546c64b816aae12c
rerun1964 <111833758+rerun1964@users.noreply.github.com>
2022-09-18 11:33:57 +0900

    L70. Rightbar@Profile CSS

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -50,8 +50,8 @@ export default function Rightbar({profile}) {
         <h4 className="rightbarTitle">ユーザー情報</h4>
         <div className="rightbarInfo">
           <div className="rightbarInfoItem">
-            <span className="rightbarInforKey">出身：</span>
-            <span className="rightbarInforKey">福岡</span>
+            <span className="rightbarInfoKey">出身：</span>
+            <span className="rightbarInfoKey">福岡</span>
           </div>
           <h4 className="rightbarTitle">あなたの友達</h4>
           <div className="rightbarFollowings">
@@ -59,7 +59,7 @@ export default function Rightbar({profile}) {
               <img 
                 src="assets/person/1.jpeg" 
                 alt="" 
-                className="rightbarFolloing"
+                className="rightbarFollowingImg"
               />
               <span className="rightbarFollowingName">Shin code</span>
             </div>
@@ -67,28 +67,28 @@ export default function Rightbar({profile}) {
               <img 
                 src="assets/person/2.jpeg" 
                 alt="" 
-                className="rightbarFolloing"
+                className="rightbarFollowingImg"
               />
               <span className="rightbarFollowingName">Yamaki</span>
             </div>            <div className="rightbarFollowing">
               <img 
                 src="assets/person/3.jpeg" 
                 alt="" 
-                className="rightbarFolloing"
+                className="rightbarFollowingImg"
               />
               <span className="rightbarFollowingName">Koga</span>
             </div>            <div className="rightbarFollowing">
               <img 
                 src="assets/person/4.jpeg" 
                 alt="" 
-                className="rightbarFolloing"
+                className="rightbarFollowingImg"
               />
               <span className="rightbarFollowingName">Matsukubo</span>
             </div>            <div className="rightbarFollowing">
               <img 
                 src="assets/person/5.jpeg" 
                 alt="" 
-                className="rightbarFolloing"
+                className="rightbarFollowingImg"
               />
               <span className="rightbarFollowingName">Kikukawa</span>
             </div>
